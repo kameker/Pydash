ae643c4631efc2e0d1012eecf8700bc05c75bbec
kameker <vsrnpra2@gmail.com>
2021-12-22 20:46:02 +0300

    пофиксил появление объектов на одних и тех же координатах и добавил орбы

--- Generation.py
+++ Generation.py
@@ -11,6 +11,7 @@ class Generator:
         self.cell_size = 50
         self.name_file = "level.txt"
         self.list_of_object = []
+        self.list_of_coords = []
 
     def render(self, screen):
         for i in range(self.width):
@@ -33,20 +34,28 @@ class Generator:
             coords = ((coords[0] - self.left) // self.cell_size, (coords[1] - self.top) // self.cell_size)
             x = coords[0] * self.cell_size + self.left
             y = coords[1] * self.cell_size + self.top
-            if data[0] == "cube":
-                pygame.draw.rect(screen, pygame.Color(255, 0, 0),
-                                 (x + 1, y + 1,
-                                  self.cell_size - 2, self.cell_size - 2))
-            elif data[0] == "spike":
-                pygame.draw.polygon(screen, pygame.Color(0, 255, 0),
-                                    ((x, y + self.cell_size), (x + self.cell_size, y + self.cell_size), (x + self.cell_size // 2, y)))
+            if (x, y) not in self.list_of_coords:
+                if data[0] == "cube":
+                    pygame.draw.rect(screen, pygame.Color(255, 0, 0),
+                                     (x + 1, y + 1,
+                                      self.cell_size - 2, self.cell_size - 2))
+                    self.list_of_coords.append((x, y))
+                elif data[0] == "spike":
+                    pygame.draw.polygon(screen, pygame.Color(0, 255, 0),
+                                        ((x, y + self.cell_size), (x + self.cell_size, y + self.cell_size),
+                                         (x + self.cell_size // 2, y)))
+                    self.list_of_coords.append((x, y))
+                elif data[0] == "orb":
+                    pygame.draw.circle(screen, pygame.Color(255, 255, 0), (x + self.cell_size // 2, y + self.cell_size // 2),
+                                       self.cell_size // 3)
+                    self.list_of_coords.append((x, y))
 
 
 size = (1000, 700)
 screen = pygame.display.set_mode(size)
 board = Generator(20, 14)
 running = True
-board.render(screen)
+# board.render(screen)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
